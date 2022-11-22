9562275c9467203af8471f673a1e59903643da40
Oleg <44002824+GilgameshH0@users.noreply.github.com>
2022-11-22 19:57:04 +0300

    1.2

--- scr.js
+++ scr.js
@@ -89,6 +89,8 @@ function sendMessage() {
         var t8 = "какие ожидания?: \n" + document.getElementById('wait').value + "\n";
         var t9 = "кошелек ETH: " + document.getElementById('eth').value + "\n";
         var t10 = "кошелек SOL: " + document.getElementById('sol').value + "\n";
+        var t11 = "Расскажи о своем опыте в НФТ и крипте,сколько ты в этой сфере, какие были твои успешные сейлы и тд: " + document.getElementById('tell').value + "\n";
+        var t12 = "Чем занимаешься на текущем рынке?: " + document.getElementById('which').value + "\n";
 
         mytext = t1 + t2 + t3 + t4 + t5 + t6 + t7 + t8 + t9 + t10
         const obj = {
